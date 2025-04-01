21748c6312d975748571307e607ba65286460496
Gren <124205591+Grenstafox@users.noreply.github.com>
2025-03-31 23:17:11 -0600

    caso2.php

--- caso2.php
+++ caso2.php
@@ -1,26 +1,24 @@
 <?php
-    $servername = "localhost";
-    $username   = "root";
-    $password   = "";
-    $db         = "marvel";
-    
-    //crear la conexion a la base de datos 
-    $conn = new mysqli($servername, $username, $password, $db);
-
-    $sql = "SELECT nombre, apellidos FROM empleados";
-    $result = mysqli_query($conn, $sql);
-
-    $contador =0;
-
-    if($result){
-        if(mysqli_num_rows($result) > 0) {
-            while($row = mysqli_fetch_assoc($result)){
-                echo ++$contador;
-                echo "Nombre: " . $row["nombre"]. "<br>";
-            }
-
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "marvel";
+ 
+//crear la conexion a la base de datos
+$conn = new mysqli($servername, $username, $password, $dbname);
+ 
+$sql = "SELECT nombre, apellidos FROM empleados";
+$result = mysqli_query($conn, $sql);
+ 
+$contador = 0;
+ 
+if($result){
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)){
+            echo ++ $contador;
+            echo "nombre:" . $row["nombre"]. "<br>", "apellidos:" . $row["apellidos"]. "<br>";
         }
-
     }
-
+}
+ 
 ?>
