3efe2c8838f7f12ee1fb9561a099d49c622461b1
Gren <124205591+Grenstafox@users.noreply.github.com>
2025-03-31 23:16:12 -0600

    abre.php

--- abre.php
+++ abre.php
@@ -1,10 +1,9 @@
 <?php
-    $conexion = new mysqli("localhost", "root", "", "marvel");
-
-    if($conexion){
-
-    }else{
-        echo "fallo algo no te conectaste";
-    }
-
+$conexion = new mysqli ("localhost","root","","marvel");
+ 
+if ($conexion) {
+}else{
+    echo "Error de conexion";
+}
+ 
 ?>
